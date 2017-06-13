1ad5ce0b1c42d9fe902872329ce76dad5918e14b
dkanojia <dkanojia93@gmail.com>
2017-06-13 10:41:11 +0530

    tournament page api model json bug fixed

--- application/models/Tournament_page_api_model.php
+++ application/models/Tournament_page_api_model.php
@@ -43,26 +43,24 @@ class Tournament_page_api_model extends CI_Model {
 
 			if(($start_date <= $today_date) && ($end_date > $today_date)){		
 			// if(($status == 1) || ($status == 2) ){		
-				$ongoing = $ongoing . '{ "tournament_id":"'.$tour_id.'", "start_date":"'.$tour_start_date.'"  , "end_date":"'.$tour_end_date.'" ,"status":"ongoing" , "banner_image":"'.$img_src.'", "tournament_name":"'.$tour_name.'", "tournament_name":"'.$tour_name.'", "tournament_sponser":"'.$tour_sponser.'", "tournament_teams":"'.$tour_teams.'"},';				
+				$ongoing = $ongoing . '{ "tournament_id":"'.$tour_id.'", "start_date":"'.$tour_start_date.'"  , "end_date":"'.$tour_end_date.'" ,"status":"ongoing" , "banner_image":"'.$img_src.'", "tournament_name":"'.$tour_name.'", "tournament_sponser":"'.$tour_sponser.'", "tournament_teams":"'.$tour_teams.'"},';				
 
 			}elseif($start_date > $today_date){
 				
-				$upcoming = $upcoming . '{ "tournament_id":"'.$tour_id.'", "start_date":"'.$tour_start_date.'"  , "end_date":"'.$tour_end_date.'" ,"status":"upcoming" , "banner_image":"'.$img_src.'", "tournament_name":"'.$tour_name.'", "tournament_name":"'.$tour_name.'", "tournament_sponser":"'.$tour_sponser.'", "tournament_teams":"'.$tour_teams.'"},';
+				$upcoming = $upcoming . '{ "tournament_id":"'.$tour_id.'", "start_date":"'.$tour_start_date.'"  , "end_date":"'.$tour_end_date.'" ,"status":"upcoming" , "banner_image":"'.$img_src.'", "tournament_name":"'.$tour_name.'", "tournament_sponser":"'.$tour_sponser.'", "tournament_teams":"'.$tour_teams.'"},';
 			}elseif($end_date < $today_date){
-				$past = $past . '{ "tournament_id":"'.$tour_id.'", "start_date":"'.$tour_start_date.'"  , "end_date":"'.$tour_end_date.'" ,"status":"highlights" , "banner_image":"'.$img_src.'", "tournament_name":"'.$tour_name.'", "tournament_name":"'.$tour_name.'", "tournament_sponser":"'.$tour_sponser.'", "tournament_teams":"'.$tour_teams.'"},';
+				$past = $past . '{ "tournament_id":"'.$tour_id.'", "start_date":"'.$tour_start_date.'"  , "end_date":"'.$tour_end_date.'" ,"status":"past" , "banner_image":"'.$img_src.'", "tournament_name":"'.$tour_name.'", "tournament_sponser":"'.$tour_sponser.'", "tournament_teams":"'.$tour_teams.'"},';
 			}			
 	    }
 		
-	    $ongoing = rtrim( $ongoing , ',');
-		$upcoming = rtrim( $upcoming , ',');
-		$past = rtrim( $past , ',');
-		$all = $past.' , '.$ongoing.' , '.$upcoming ;
-
+	    // $ongoing = rtrim( $ongoing , ',');
+		// $upcoming = rtrim( $upcoming , ',');
+		// $past = rtrim( $past , ',');
+		$all = $past.'  '.$ongoing.'  '.$upcoming ;
+		$all = rtrim( $all, ',');
 		$all = '['.$all.']';
 	
-		// $data = '{"highlights": '.$past.' , "ongoing": '.$ongoing.' , "upcoming": '.$upcoming.'}';
 		$data = '"all_tournament": '.$all.' ';
-		// echo '{"response":"true" , "data": '.$data.'}';
 		echo '{"response":"true" , '.$data.'}';
 	}
 	//echo $res;
